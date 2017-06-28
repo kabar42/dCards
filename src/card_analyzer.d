10fdf34c5539e20f0ff4394630d9a3004938c39a
Kent Barnett <barnettkent@gmail.com>
2017-06-27 21:52:36 -0500

    Supporting code for hand analysis

--- src/card_analyzer.d
+++ src/card_analyzer.d
@@ -13,9 +13,11 @@ enum HandType {
     RoyalFlush
 }
 
+const int HAND_TYPES_COUNT = 10;
+
 struct HandData {
-    int[4] suit_count;
-    int[13] rank_count;
+    int[SUIT_COUNT] suit_count;
+    int[RANK_COUNT] rank_count;
 }
 
 int[HandType] count_hand_types(Hand[] all_hands) {
@@ -36,3 +38,42 @@ HandData get_hand_data(Hand hand) {
     }
     return data;
 }
+
+Rank[] get_ranks_present(int[] counts) {
+    auto ranksPresent = new Rank[0];
+    foreach(i ; counts) {
+        if(counts[i] > 0) {
+            auto new_rank = new Rank[1];
+            new_rank[0] = intToRank(i);
+            ranksPresent ~= new_rank;
+        }
+    }
+    return ranksPresent;
+}
+
+bool ranksAreSequential(Rank[] ranks) {
+    if(ranks.length < Hand.hand_size) {
+        return false;
+    }
+
+    // ranks.sort();
+
+    if(ranks[0] == Rank.Ace &&
+       ranks[1] == Rank.Ten &&
+       ranks[2] == Rank.Jack &&
+       ranks[3] == Rank.Queen &&
+       ranks[4] == Rank.King) {
+        return true;
+    }
+
+    foreach(i ; ranks[1..$]) {
+        auto prevRank = int(ranks[i-1]);
+        auto thisRank = int(ranks[i]);
+        if(prevRank != thisRank-1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
--- src/cards.d
+++ src/cards.d
@@ -3,12 +3,74 @@ import std.string;
 import std.traits;
 
 enum Rank {
-    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    Ace = 0, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Invalid
+}
+
+const int RANK_COUNT = 13;
+
+int opCmp(ref const Rank me, ref const Rank r) {
+    int result = 0;
+    if(int(me) < int(r)) {
+        result = -1;
+    } else if(int(me) > int(r)) {
+        result = 1;
+    }
+    return result;
+}
+
+Rank intToRank(int r) {
+    auto result = Rank.Invalid;
+    switch(r) {
+        case 0:
+            result = Rank.Ace;
+            break;
+        case 1:
+            result = Rank.Two;
+            break;
+        case 2:
+            result = Rank.Three;
+            break;
+        case 3:
+            result = Rank.Four;
+            break;
+        case 4:
+            result = Rank.Five;
+            break;
+        case 5:
+            result = Rank.Six;
+            break;
+        case 6:
+            result = Rank.Seven;
+            break;
+        case 7:
+            result = Rank.Eight;
+            break;
+        case 8:
+            result = Rank.Nine;
+            break;
+        case 9:
+            result = Rank.Ten;
+            break;
+        case 10:
+            result = Rank.Jack;
+            break;
+        case 11:
+            result = Rank.Queen;
+            break;
+        case 12:
+            result = Rank.King;
+            break;
+        default:
+            break;
+    }
+    return result;
 }
 
 enum Suit {
     Hearts, Clubs, Diamonds, Spades
 }
+
+const int SUIT_COUNT = 4;
 
 struct Card {
     Rank rank;
@@ -33,10 +95,10 @@ Card[] createStandardDeck() {
 
 class Hand {
 private:
-    const int hand_size = 5;
     int card_count;
 
 public:
+    static int hand_size = 5;
     Card[] cards;
 
     this() {
